1f2cdc4531a4856c31efe5fe2c11b71f95e92524
Hoang Mach It <hoangmach.website@gmail.com>
2023-05-25 00:24:24 +0700

    first commit

---
+++ pages/auth/login/index.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  setup() {
+    useHead({
+      titleTemplate: "Auth login",
+      bodyAttrs: {
+        class: "login",
+      },
+    });
+    return { dynamic: ref(49) };
+  },
+  head: {
+    title: "Another title",
+  },
+};
+</script>
+<template>
+  <div>
+    <h1>Login Page</h1>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  setup() {
+    useHead({
+      titleTemplate: "useHead example",
+      bodyAttrs: {
+        class: "home",
+      },
+    });
+    return { dynamic: ref(49) };
+  },
+  head: {
+    title: "Another title",
+  },
+};
+</script>
+<template>
+  <div>
+    <h1>Index Page</h1>
+    <nuxt-link to="auth/login">Login Page</nuxt-link>
+  </div>
+</template>
